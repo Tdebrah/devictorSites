2f2901ff51d8a8e285681e80b773c0c878e9c303
TUMUKURATE DEBRAH <76787688+Tdebrah@users.noreply.github.com>
2021-07-29 18:12:52 +0300

    My first hosted site

---
+++ sky hi.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // $("button").click(function() {
+    $(".Experience").animate({ left: '800px' });
+    $(".Experience").fadeOut();
+    // $("#div2").fadeOut("slow");
+    // $("#div3").fadeOut(3000);
+    // });
+});
+$(document).ready(function() {
+
+    $("#passionate").show();
+});
